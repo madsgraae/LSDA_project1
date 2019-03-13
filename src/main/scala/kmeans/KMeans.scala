c67023efe318d17fcb9a6883c7940e916702980d
macg <macg@itu.dk>
2019-03-13 17:40:02 +0100

    Try

--- src/main/scala/kmeans/KMeans.scala
+++ src/main/scala/kmeans/KMeans.scala
@@ -10,29 +10,27 @@ class KMeans {
   // Hint: Use the Seed to generate random numbers as in DataGenerator.scala.
   //       Select 'k' random points from the given points
   
-  // """RET"""
   def initializeMeans(k: Int, points: Seq[Point], seed:Int): Seq[Point] = {
-    val random = new Random(seed)
-    (0 until k).map(_ => points(random.nextInt(points.length))).to[mutable.ArrayBuffer]
+    //val l = Random.shuffle(points)
+    //l.take(k)
+    // Random.shuffle(points).take(k)
+    points.tail.take(k)
+
   }
+
 
   // Find the cluster ("means") that are closest to the point p. 
   // Hint: squareDistance is defined in Point
   // """RET"""
   def findClosest(p: Point, means: GenSeq[Point]): Point = {
-    //assert(means.size > 0)
-    var closest = means(0)
-    var i = 1
-    var minDist = p.squareDistance(means(0))
-    while (i < means.length) {
-      val dist = p.squareDistance(means(i))
-      if (dist < minDist) {
-        minDist = dist
-        closest = means(i)
+    var closestMean = means.head
+    
+    for (mean <- means){
+      if (p.squareDistance(mean) < p.squareDistance(closestMean)){
+        closestMean = mean
       }
-      i += 1
     }
-    closest
+    closestMean
   }
 
   // Cluster all points
@@ -40,49 +38,39 @@ class KMeans {
   // Remember to handle empty cases.
   // """RET"""
   def classify(points: GenSeq[Point], means: GenSeq[Point]): GenMap[Point, GenSeq[Point]] = {
-  // group each point by the mean. Should be enough, but needs to return empty lists for unused means
-  val pointsMeanMap = points.par.groupBy(findClosest(_, means))
-  // So iterate over means get (empty) list and return map
-      means.par.map(mean => mean -> pointsMeanMap.getOrElse(mean, GenSeq())).toMap
-    }
+    val dict = points.groupBy(findClosest(_, means))
+    // So iterate over means get (empty) list and return map
+    means.map(mean => mean -> dict.getOrElse(mean, GenSeq())).toMap
+  }
 
   // Find average of points.
   // If the sequence of points is empty, return oldMean.
   // """RET"""
   def findAverage(oldMean: Point, points: GenSeq[Point]): Point = {
-    println(oldMean)
-    new Point(1, 2)
-
-    
-    
-    
-    
+   ???
   }
 
   // Get average of classification (using findAverage function) and update old cluster
   // """RET"""
   def update(classified: GenMap[Point, GenSeq[Point]], oldMeans: GenSeq[Point]): GenSeq[Point] = {
-      oldMeans.par.map(oldMean => findAverage(oldMean, classified(oldMean)))
+    ???
   }
 
   // Check if the sum of the distance between the old and the new clusters are less than eta
   // """RET"""
   def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
-      (oldMeans zip newMeans).forall{
-  case (oldMean, newMean) => oldMean.squareDistance(newMean) <=  eta
-      }
-    }
-
+  ???
+  }
   // Clusters all points with initial cluster means
   // The function returns when the clusters converge less than eta
-  @tailrec
-  final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
-    val classified = ???
-    val newMeans = ???
-    if (???) 
-      ???  // Must be tail recursive
-    else newMeans 
-  }
+//  @tailrec
+//  final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
+//    val classified = ???
+//    val newMeans = ???
+//    if (???) 
+//      ???  // Must be tail recursive
+//    else newMeans 
+
 }
 
 object KMeansRunner {
@@ -98,10 +86,10 @@ object KMeansRunner {
   // Loads dataset and executes kmeans
   def main(args: Array[String]) {
     // Prepare dataset
-    val numPoints = 500000
+    val numPoints = 10
     val numDimensions = 3
     val eta = 0.01
-    val k = 32 // Number of clusters
+    val k = 5 // Number of clusters
     val seed = 7
 
     // You can choose between the two datasets in the comments below. 
@@ -117,18 +105,30 @@ object KMeansRunner {
     // Initialize a KMeans object and initialize means
     val kMeans = new KMeans()
     val means = kMeans.initializeMeans(k, points, seed)
+    
+    // ---------------------------- //
 
+    // val testPoint = points.head
+    // val closest = kMeans.findClosest(testPoint, means)
+    val classified = kMeans.classify(points, means)
+    val (key, value) = classified.head
+    var newMean = kMeans.findAverage(key, value)
+    //val oldMean = means.head
+
+    // ----------------------------------/
+    
     // Execute kMeans sequentially and time the execution
-    time{
-      kMeans.kMeans(points, means, eta)
-    }
+    //time{
+      //kMeans.kMeans(points, means, eta)
+    //}
 
     // Execute kMeans in parallel and time the execution
-    time{
-      val parPoints = points.par
-      val parMeans = means.par
-      kMeans.kMeans(parPoints, parMeans, eta)
-    }
+    //time{
+      //val parPoints = points.par
+      //val parMeans = means.par
+      //kMeans.kMeans(parPoints, parMeans, eta)
+    //}
+
   }
 
 }
